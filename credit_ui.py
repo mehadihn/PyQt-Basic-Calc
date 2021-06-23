b407cc13741c7f2940d70a42770cee83fb74e3c9
Mehadi Hassan <mehadihassan116@gmail.com>
2021-06-23 16:11:50 +0600

    credit

--- credit_ui.py
+++ credit_ui.py
@@ -1,15 +1,13 @@
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QMainWindow
-import calc
+import credit
 
-class calcWindow(QMainWindow):
+class creditWindow(QMainWindow):
     def __init__(self):
-        print("In Calc UI")
         QMainWindow.__init__(self)
-        self.ui = calc.Ui_MainWindow()
+        self.ui = credit.Ui_credit()
         self.ui.setupUi(self)
-        # self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
-        # self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
 
     def show_window(self):
         self.show()
--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import sys
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import calc_ui
+import calc_ui, credit_ui
 
 class calculator(QMainWindow):
 
     def __init__ (self):
         QMainWindow.__init__(self)
+
         self.calc = calc_ui.calcWindow()
+        self.credit = credit_ui.creditWindow()
+
+        #self.calc.ui.cre.clicked.connect(self.credit)
         self.calc.ui.add.clicked.connect(self.add_button)
         self.calc.ui.sub.clicked.connect(self.sub_button)
         self.calc.ui.multiply.clicked.connect(self.multiply_button)
         self.calc.ui.Div.clicked.connect(self.div_button)
+
+        self.calc.ui.cre.clicked.connect(self.credit_button)
+        self.credit.ui.goBack.clicked.connect(self.goBack_button)
+
+    def credit_button(self):
+        self.credit.ui.credit_name.setText("Mehadi Hassan")
+        self.calc.hide_window()
+        self.credit.show_window()
+
+    def goBack_button(self):
+        self.credit.hide_window()
+        self.calc.show_window()
+
 
     def add_button(self):
         num01 = int(self.calc.ui.num01.text())
@@ -46,14 +63,18 @@ class calculator(QMainWindow):
     def show_calculator_window(self):
         self.calc.show()
 
+    def show_credit_window(self):
+        self.credit.show()
+
     def initialize_and_show(self):
         self.show_calculator_window()
-        #self.populate_port_list()
+        #self.show_credit_window()
 
 
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
     calculator_window = calculator()
+    #calculator_window.calc.show()
     calculator_window.initialize_and_show()
     sys.exit(app.exec_())
